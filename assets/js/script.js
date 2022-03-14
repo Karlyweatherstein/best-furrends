dd1d91e22da8fba43b9492807522d01d4e3da902
unknown <arestre@ncsu.edu>
2022-03-14 19:24:14 -0400

    working on api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,44 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+var dogList = document.querySelector('ul');
+var fetchButton = document.getElementById('fetch-button');
+
+
+function getDogs() {
+    var requestOptions = {
+        token: "91B7D3A43B5C31EA",
+        method: 'GET',
+        redirect: 'follow'
+      };
+      
+      fetch("https://api.findadogfor.me/api/shelter/TX1399", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+   // var requestUrl = "https://api.petfinder.com/v2/animals?type=dog&page=2"
+
+
+ //   fetch(requestUrl)
+ //   .then(function(response) {
+  //      return response.json();
+  //  })
+  //  .then(function(data) {
+  //      console.log (data);
+  //      for (var i = 0; i < data.length; i++) {
+  //        var listItem = document.createElement('li');
+   //       listItem.textContent = data[i];
+   //       dogList.appendChild(listItem);
+   //     }
+    //  });
+}
+
+fetchButton.addEventListener("click", getDogs);
+
+
+//pet finder api
+//api key is iWBmcogOlYqjvHonpYCUHWST24S2curEA8jvFktWDTRBfjBc1O
+//api secret? CgFV9iPUIvZ6W2dRFLIm1w5A2YQVpeuOikrcZprP
+
+//curl -d "grant_type=client_credentials&client_id=iWBmcogOlYqjvHonpYCUHWST24S2curEA8jvFktWDTRBfjBc1O&client_secret=CgFV9iPUIvZ6W2dRFLIm1w5A2YQVpeuOikrcZprP" https://api.petfinder.com/v2/oauth2/token
