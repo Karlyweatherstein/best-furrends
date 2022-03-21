51cb22b72564c752ee2407e6e0c7caee4d700591
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-03-20 21:21:58 -0400

    merge files

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,6 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
-  click()
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -26,15 +25,32 @@ window.onclick = function(event) {
 }
 
 
-const url = "https://api.rescuegroups.org/v5/public/animals/search/available/";
+var searchButton = document.getElementById("searchbtn");
+
+searchButton.onclick = function(){
+    getData()
+};
+
+var btn = document.getElementById("searchbtn");
+btn.onclick = function() {
+    click()
+}
+
+
+const url = "https://api.rescuegroups.org/v5/public/animals/search/available/dogs/";
+
 
 async function getData() {
+    var locationInput = document.getElementById("modalLocation");
+ var zipcode = locationInput.value.trim();
+  
     var raw = JSON.stringify({
           "data": {
             "filterRadius": {
-              "miles": 100,
-              "postalcode": 27527
-            }}});
+              "miles": 50,
+              "postalcode": zipcode
+            }
+        }});
     const response = await fetch(url, {
         method: 'POST',
         headers: {
@@ -46,6 +62,7 @@ async function getData() {
    });
    const data = await response.json();
    console.log(data.data);
+
 }
 
 function click() {
@@ -54,5 +71,39 @@ function click() {
 
 
 
+//Start of Fact API 
 
+var factsArr = JSON.parse(localStorage.getItem("dogData")) || [];
+
+function funFacts () {
+  fetch('https://api.thedogapi.com/v1/images/search/?x-api-key=c15bb368-37c9-4f47-8908-f8d0fe483813/breeds').then(function (response) {
+      // The API call was successful!
+      return response.json();
+    }).then(function (data) {
+      console.log(data);
+      factsArr.push(data)
+      localStorage.setItem("dogData", JSON.stringify(factsArr));
+      var displayData = document.getElementById('funFactsHere');
+      displayData.innerHTML = factsArr
+      // var infoEl = document.createElement('p');
+      // infoEl.innerText = 'Weight: ${factsArr.weight}';
+      // displayData.append(infoEl);
+      clearArr()
+
+
+
+  
+
+    }).catch;
+
+}
+
+nextBtn.addEventListener('click', funFacts)
+ 
+function clearArr(){
+  factsArr = []
+}
+
+
+//End of fact API
 
